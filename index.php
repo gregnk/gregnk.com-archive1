0b214d914197cdd26ce250f4ee4be78aa12ace24
Gregory <Gregory@192.168.0.193>
2015-07-01 19:48:00 -0400

    Added template to index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php echo file_get_contents("header_template.html")?>
+                <h1>Welcome</h1>
+                <p>Hi, im Greg and this is my site. Since there is almost nothing here yet, feel free to walk around.</p>
+                <!-- upadtes -->
+                <br>
+                <h1 id="updates">Updates</h1>
+                <p>2015-1-28 : Website Re-Designed.</p>
+                <br>
+                <!-- software -->
+                <h1 id="software">Software</h1>
+                <em>soon</em>
+                <br>
+<?php echo file_get_contents("footer_template.html")?>
